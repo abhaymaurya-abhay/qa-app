6f9668f01e4916e5bfa487b16fd9c7dd1fe81190
abhaymaurya-abhay <computerabhaymaurya@gmail.com>
2025-04-11 06:01:36 +0530

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,13 +1,15 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('v1').then(cache => {
-    return cache.addAll([
-      '/',
-    ]);
-  }));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("qna-cache").then(cache => {
+      return cache.addAll(["./", "index.html"]);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
